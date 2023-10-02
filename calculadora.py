cd62b35d6973b8494808f564f591403f16ead4be
zx21student016 <zx21student016@colegiovalledelmiro.es>
2023-10-02 12:04:18 +0200

    division

---
+++ calculadora.py
@@ -0,0 +1,18 @@
+x = int(input("introduce el primer numero:"))
+z = int(input("introduce el segundo numero:"))
+
+sumar = x + z
+
+restar = x + z
+
+multiplicar = x + z
+
+division = x / z
+
+print ("la suma es =", sumar)
+
+print ("la resta es =", restar)
+
+print ("la multiplicacion es =", multiplicar)
+
+print ("la division es =", division)
